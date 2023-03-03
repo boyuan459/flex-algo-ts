8b64665a605dd679766a724b54f988fc62ddbd80
boyuan <boyuan954@gmail.com>
2023-03-03 22:03:40 +1100

    add binary tree

---
+++ lib/binaryTree.d.ts
@@ -0,0 +1,12 @@
+declare class BinaryNode {
+    value: number;
+    left: BinaryNode | null;
+    right: BinaryNode | null;
+    constructor(value: number);
+}
+export declare class BinaryTree {
+    root: BinaryNode | null;
+    constructor();
+    insert(data: (number | null)[]): BinaryNode | null | undefined;
+}
+export {};
