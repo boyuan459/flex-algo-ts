51f4936af49bc39a71d909870fac983f29204a6d
boyuan <boyuan954@gmail.com>
2023-03-13 23:08:20 +1100

    export quick select

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import { PriorityQueue } from './priorityQueue';
 import { BinaryTree, BinaryNode } from './binaryTree';
 import { BST } from './bst';
-import { quickSort, pivot } from './quickSort';
-export { PriorityQueue, BinaryTree, BinaryNode, BST, quickSort, pivot };
+import { quickSort, pivot, quickSelect, quickSelectPivot } from './quickSort';
+export { PriorityQueue, BinaryTree, BinaryNode, BST, quickSort, pivot, quickSelect, quickSelectPivot, };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { PriorityQueue } from './priorityQueue';
 import { BinaryTree, BinaryNode } from './binaryTree';
 import { BST } from './bst';
-import { quickSort, pivot } from './quickSort';
+import { quickSort, pivot, quickSelect, quickSelectPivot } from './quickSort';
 
-export { PriorityQueue, BinaryTree, BinaryNode, BST, quickSort, pivot };
+export {
+  PriorityQueue,
+  BinaryTree,
+  BinaryNode,
+  BST,
+  quickSort,
+  pivot,
+  quickSelect,
+  quickSelectPivot,
+};
